0af9e0f0d93df9027e7c47c483c68e277dcb3d20
unknown <pandrien@hmc>
2015-04-12 19:34:26 -0700

    half of english text added to about page

--- webroot/about.php
+++ webroot/about.php
@@ -20,32 +20,32 @@
 	<div id="About">
 		<p></p>
 		<img class="cimg" SRC="Park.jpg">
+		<p>We met in 2010 at Harvey Mudd College during a summer program for new students called Summer Institute. Summer Institute was packed with lots of fun activities designed to help new students get to know each other and acclimate to college life.</p>
 		
-		<p></p>	
 		<img class="cimg" SRC="Boojum2.jpg">
+		<p>We have especially fond memories of a trip that we took during Summer Institute to a ropes course called Boojum, where we did team building activities including swinging on a rope and balancing on a wire high up in the trees.</p>
 		
-		<p></p>
 		<img class="cimg" SRC="HvZ1.jpg">
+		<p>As our friendship grew, one of our favorite activities was a week-long game that happened every semester called Humans vs. Zombies, where humans try to defend themselves using toy Nerf Blasters. Right before the final mission of the game we snapped a photo in all our gear. Out of 8 games, we each survived to the final mission as humans 3 times.</p>
 		
-		<p></p>
 		<img class="cimg" SRC="tiedye.jpg">
+		<p>As we slowly fell in love, we bonded over active college games and activities, including “Battle Tie Dye”, an annual event where students color white t-shirts with dye soaked pool toys.</p>
 		
-		<p></p>
 		<img class="cimg" SRC="masquerade.png">
-		
 		<p></p>
+		
 		<img class="cimg" SRC="BlackAndWhiteBall.png">
-		
 		<p></p>
+		
 		<img class="cimg" SRC="prom2012.png">
-		
 		<p></p>
+		
 		<img class="cimg" SRC="Engagement1.jpg">
-		
 		<p></p>
+		
 		<img class="cimg" SRC="Grad1.jpg">
-		
 		<p></p>
+		
 		<img class="cimg" SRC="Park2.jpg">
 	</div>
 </div>
